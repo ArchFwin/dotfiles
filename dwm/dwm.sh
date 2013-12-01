0a2ced2f6d2ca5e2f6b001ed4158cdedaef026b1
Andrew Webley <UnsuspectingHero@gmail.com>
2013-12-01 12:48:27 +0000

    Updated pkglists, updated dwm.sh to allow three login-types and to use an autostart array instead of an autostart list. Also slimlined how the login types behave.

--- dwm/dwm.sh
+++ dwm/dwm.sh
@@ -1,112 +1,93 @@
 #!/bin/sh
 #Maintainer = Andrew Webley <UnsuspectingHero@gmail.com>
-#DWM startup file.
-#This includes all the things that are automatically started, as well as a few configuration options
-#That should extend to all of the various autostart directories.
-xrdb -merge ~/.Xresources
-#Autostart Section
+#DWM startup script
+
+#All program lines should end in an & except the last one. Any lines without a & cause issues (until the offending program is killed/exits, nothing else runs).
+
+#Include various options set in the ~/.Xresources file
+xrdb -merge "$HOME"/.Xresources &
+
+# Start system autostart files/scripts if they exist
+
 if [[ -d /etc/X11/xinit/xinitrc.d ]]; then
   for f in /etc/X11/xinit/xinitrc.d/*; do
-    [[ -x "$f" ]] && . "$f"
+    [[ -x "$f" ]] && . "$f" &
   done
   unset f
-fi &
+fi
 
-#Xdg Autostart Compliance
-<<<<<<< HEAD
-#It's unlikely that a .desktop file will mention dwm, so the possibility is ignored.
-=======
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
 if [[ -d /etc/xdg/autostart ]]; then
 	for y in /etc/xdg/autostart/*; do
 		x=$(cat $y)
-		case $x in
-<<<<<<< HEAD
-=======
-		#It's unlikely that a .desktop file will mention dwm, so the possibility is ignored.
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
+		case "$x" in
 		*OnlyShowIn*)
+			#There ain't no-one in the 'verse putting "OnlyShowIn=dwm" in a .desktop file.
+			#Similarly, ain't no-one putting "NotShowIn=dwm" in a .desktop file. Hence, both cases are ignored.
 			;;
 		*)
-			sh -c "$(echo $x | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
+			#Desktop file parsing.
+			sh -c "$(echo "$x" | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
 			;;
 		esac
-		unset x
 	done
 	unset y
-<<<<<<< HEAD
+	unset x
 fi
-=======
-fi &
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
-#Separated to prevent failure if one of the two directories doesn't exist
-if [[ -d $HOME/.config/autostart ]]; then
-	for n in $HOME/.config/autostart/*; do
-		m=$(cat $n)
-		case $m in
-		*OnlyShowIn*)
-			;;
-		*)
-			sh -c "$(echo $m | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
-			;;
-		esac
+
+#Default empty autostart for clean
+autostart=('')
+
+#Add items to autostart for full only
+[[ "$1" = "full" ]] && autostart +=('firefox-ux' 'spotify' 'sakura')
+
+#partial/full autostart loop
+if [[ "$1" = "partial" || "$1" = "full" ]]; then
+	#Autostart items in full and partial
+	autostart+=('pidgin' 'claws-mail' 'transmission-gtk -m')
+	#Secondary autostart directory, nothing important started here so okay to skip in clean for minimal session
+	if [[ -d $HOME/.config/autostart ]]; then
+		for n in $HOME/.config/autostart/*; do
+			m=$(cat $n)
+			case $m in
+			*OnlyShowIn*)
+				;;
+			*)
+					sh -c "$(echo "$m" | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
+				;;
+			esac
+		done
+		unset n
 		unset m
+	fi
+	#Runs everything in the autostart array
+	for x in "${autostart[@]}"; do
+		$x &
 	done
-	unset n
-<<<<<<< HEAD
-fi
-
-#Autostart section to allow multiple .desktop files to use same script
-if [[ "$1" != "clean" ]]; then
-	pidgin &
-	claws-mail &
-	dropboxd &
-	SpiderOak &
-	transmission-gtk -m &
-fi
-=======
-fi &
-
-pidgin &
-claws-mail &
-dropboxd &
-SpiderOak &
-transmission-gtk -m &
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
 
 #Power Management Script
-$HOME/scripts/bat.sh &
+$HOME/Scripts/bat.sh &
 
 #Configuration Section
-setxkbmap gb
+setxkbmap -layout gb -option terminate:ctrl_alt_bksp -option compose:menu
 # http://i.imgur.com/U9QgGDY.jpg
-feh --bg-scale /home/andrew/Pictures/city.jpg
-<<<<<<< HEAD
+feh --bg-scale $HOME/Pictures/city.jpg
 $HOME/Scripts/redshift &
-
-#Things that need to run in all sessions
 gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh &
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
-
-#Only start nm-applet if network-manager is running
-[[ `pgrep NetworkManager` ]] && nm-applet &
-=======
-/home/andrew/Scripts/redshift &
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
 
 #Automatic screen-lock and suspend
 xautolock -time 5 -locker "slock" -killer "systemctl suspend" -killtime 10 -detectsleep &
 
-#Conky status bar
-#conky | while read -r; do xsetroot -name "$REPLY"; done &
+#Dzen is set to be slightly further left than the end of the systray. This simply hides ``dwm-6.0'' from view.
+xsetroot -name " "
 
 #dzen2 status bar
 fgcolour="#0AA6CF"
-bgcolour="#1a1a1a"
-font="xft:Fira Mono:size=8"
+bgcolour="#272822"
+font="Fira Mono:size=8"
 dzevents="key_Escape=exit;sigusr1=raise;sigusr2=lower;"
-xsetroot -name " "
-conky -c .conkyrcdzen | dzen2 -x "524" -h "13" -w "750" -ta r -fg "${fgcolour}" -bg "${bgcolour}" -fn "${font}" -e "${dzevents}" &
+
+conky -c .conkyrc | dzen2 -x "604" -h "13" -w "650" -ta r -fg "${fgcolour}" -bg "${bgcolour}" -fn "${font}" -e "${dzevents}" &
 
 #Dzen starts before dwm, so this is used to make dzen go over the dwm bar
 sleep 10s && kill -USR1 `pgrep dzen` &
--- scripts/dwm.sh
+++ scripts/dwm.sh
@@ -1,112 +1,93 @@
 #!/bin/sh
 #Maintainer = Andrew Webley <UnsuspectingHero@gmail.com>
-#DWM startup file.
-#This includes all the things that are automatically started, as well as a few configuration options
-<<<<<<< HEAD
+#DWM startup script
 
-#Two if clauses to make dwm more compliant with how some people believe desktop environments should behave.
-if [ -d /etc/X11/xinit/xinitrc.d ]; then
-	for f in /etc/X11/xinit/xinitrc.d/*; do
-		[ -x "$f" ] && . "$f"
-	done
-	unset f
-fi &
-#This parses the .desktop files in one of two autostart directories and executes them.
-#/etc/xdg/autostart isn't included because it makes a complicated loop that has to check whether something should be started (i.e, finds "OnlyShowIn=KDE" or other similar things, and then ignores/runs them as appropriate).
-if [ -d /home/andrew/.config/autostart ]; then
-	for y in /home/andrew/.config/autostart/*; do
-    	sh -c "$(cat $y | grep -m 1 "Exec" | sed 's/^Exec=//' | sed 's/ %.//')" &
-	done
-	unset y
-fi &
+#All program lines should end in an & except the last one. Any lines without a & cause issues (until the offending program is killed/exits, nothing else runs).
 
-#Autostart Section
-btpd -p 6161 --max-peers 120 &
-pidgin &
-claws-mail &
-/usr/lib/lxpolkit/lxpolkit &
-xautolock -time 5 -locker "slock" -killer "systemctl suspend" -killtime 10 -detectsleep &
-=======
-#That should extend to all of the various autostart directories.
-xrdb -merge ~/.Xresources
-#Autostart Section
+#Include various options set in the ~/.Xresources file
+xrdb -merge "$HOME"/.Xresources &
+
+# Start system autostart files/scripts if they exist
+
 if [[ -d /etc/X11/xinit/xinitrc.d ]]; then
   for f in /etc/X11/xinit/xinitrc.d/*; do
-    [[ -x "$f" ]] && . "$f"
+    [[ -x "$f" ]] && . "$f" &
   done
   unset f
-fi &
+fi
 
-#Xdg Autostart Compliance
 if [[ -d /etc/xdg/autostart ]]; then
 	for y in /etc/xdg/autostart/*; do
 		x=$(cat $y)
-		case $x in
-		#It's unlikely that a .desktop file will mention dwm, so the possibility is ignored.
+		case "$x" in
 		*OnlyShowIn*)
+			#There ain't no-one in the 'verse putting "OnlyShowIn=dwm" in a .desktop file.
+			#Similarly, ain't no-one putting "NotShowIn=dwm" in a .desktop file. Hence, both cases are ignored.
 			;;
 		*)
-			sh -c "$(echo $x | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
+			#Desktop file parsing.
+			sh -c "$(echo "$x" | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
 			;;
 		esac
-		unset x
 	done
 	unset y
-fi &
-#Separated to prevent failure if one of the two directories doesn't exist
-if [[ -d $HOME/.config/autostart ]]; then
-	for n in $HOME/.config/autostart/*; do
-		m=$(cat $n)
-		case $m in
-		*OnlyShowIn*)
-			;;
-		*)
-			sh -c "$(echo $m | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
-			;;
-		esac
-		unset m
-	done
-	unset n
-fi &
+	unset x
+fi
 
-pidgin &
-claws-mail &
-dropboxd &
-SpiderOak &
-transmission-gtk -m &
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
+#Default empty autostart for clean
+autostart=('')
+
+#Add items to autostart for full only
+[[ "$1" = "full" ]] && autostart +=('firefox-ux' 'spotify' 'sakura')
+
+#partial/full autostart loop
+if [[ "$1" = "partial" || "$1" = "full" ]]; then
+	#Autostart items in full and partial
+	autostart+=('pidgin' 'claws-mail' 'transmission-gtk -m')
+	#Secondary autostart directory, nothing important started here so okay to skip in clean for minimal session
+	if [[ -d $HOME/.config/autostart ]]; then
+		for n in $HOME/.config/autostart/*; do
+			m=$(cat $n)
+			case $m in
+			*OnlyShowIn*)
+				;;
+			*)
+					sh -c "$(echo "$m" | grep -m 1 "Exec=" | sed 's/Exec=//g')" &
+				;;
+			esac
+		done
+		unset n
+		unset m
+	fi
+	#Runs everything in the autostart array
+	for x in "${autostart[@]}"; do
+		$x &
+	done
 
 #Power Management Script
-/home/andrew/scripts/bat.sh &
+$HOME/Scripts/bat.sh &
 
 #Configuration Section
-setxkbmap gb
-<<<<<<< HEAD
-feh --bg-scale /home/andrew/Pictures/city.png
-=======
+setxkbmap -layout gb -option terminate:ctrl_alt_bksp -option compose:menu
 # http://i.imgur.com/U9QgGDY.jpg
-feh --bg-scale /home/andrew/Pictures/city.jpg
-/home/andrew/Scripts/redshift &
+feh --bg-scale $HOME/Pictures/city.jpg
+$HOME/Scripts/redshift &
+gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh &
+export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
 #Automatic screen-lock and suspend
 xautolock -time 5 -locker "slock" -killer "systemctl suspend" -killtime 10 -detectsleep &
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
 
-#Conky status bar
-#conky | while read -r; do xsetroot -name "$REPLY"; done &
+#Dzen is set to be slightly further left than the end of the systray. This simply hides ``dwm-6.0'' from view.
+xsetroot -name " "
 
 #dzen2 status bar
 fgcolour="#0AA6CF"
-bgcolour="#1a1a1a"
-font="xft:Fira Mono:size=8"
+bgcolour="#272822"
+font="Fira Mono:size=8"
 dzevents="key_Escape=exit;sigusr1=raise;sigusr2=lower;"
-<<<<<<< HEAD
-xsetroot -name " " &
-conky -c "$HOME/.conkyrcdzen" | dzen2 -x "740" -h "13" -w "800" -ta r -fg ${fgcolour} -bg ${bgcolour} -fn ${font} -e ${dzevents} &
-=======
-xsetroot -name " "
-conky -c .conkyrcdzen | dzen2 -x "524" -h "13" -w "750" -ta r -fg "${fgcolour}" -bg "${bgcolour}" -fn "${font}" -e "${dzevents}" &
->>>>>>> 54557b780ce9ca0e73a607b0b54bbfd8b1048361
+
+conky -c .conkyrc | dzen2 -x "604" -h "13" -w "650" -ta r -fg "${fgcolour}" -bg "${bgcolour}" -fn "${font}" -e "${dzevents}" &
 
 #Dzen starts before dwm, so this is used to make dzen go over the dwm bar
 sleep 10s && kill -USR1 `pgrep dzen` &
